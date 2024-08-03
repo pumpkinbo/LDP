67d8ac0fe5be788eea41e00388d0b9dc856ba39e
pumpkinbo <724102400@qq.com>
2024-08-03 20:57:07 +0800

    Normalization

--- countermeasures/norm.py
+++ countermeasures/norm.py
@@ -9,15 +9,14 @@ import pandas as pd
 from defense import DefenseMethod
 from frequency.LDPprotocol import LDPProtocol
 
-class Normalization(DefenseMethod):
-    def apply_defense(self, protocol : LDPProtocol,original_fre : dict, attacked_fre : dict):
+class Normalization():
+    def cal_overall_gain(self, original_fre : dict, attacked_fre : dict, target_items : list):
         """
         Apply normalization to the frequencies.
         :param original_fre: Dictionary of original frequencies
         :param attacked_fre:Dictionary of attacked frequencies
         :return: Dictionary of normalized frequencies
         """
-        self.protocol = protocol
         ori_min_fre = min(original_fre.values())
         att_min_fre = min(attacked_fre.values())
         ori_adjusted_fre = {k: v - ori_min_fre for k, v in original_fre.items()}
@@ -27,11 +26,9 @@ class Normalization(DefenseMethod):
         ori_normalized_fre = {k: v / total_ori_adj for k,v in ori_adjusted_fre.items()}
         att_normalized_fre = {k: v / total_att_adj for k,v in att_adjusted_fre.items()}
 
-        return ori_normalized_fre, att_normalized_fre
-
-    def cal_overall_gain(self, normalized_ori_fre : dict, normalized_att_fre : dict, target_items : list):
         overall_gain = 0
+
         for item in target_items:
-            gain = normalized_att_fre.get(item, 0) - normalized_ori_fre.get(item, 0)
+            gain = att_normalized_fre.get(item, 0) - ori_normalized_fre.get(item, 0)
             overall_gain += gain
         return overall_gain
