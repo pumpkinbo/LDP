963760e649340512a0d76e0e51edf2b1b7f4715f
pumpkinbo <724102400@qq.com>
2024-08-05 18:16:13 +0800

    useless file

--- theoretical analysis/1.py
+++ theoretical analysis/1.py
@@ -1,40 +1,91 @@
-import collections
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+#
+# yticks = [0, 10**-2, 10**-1,10**0, 10**1]
+# # yticks = np.linspace(min(yticks), max(yticks), 5)
+# yticklabels=[r'$0$',r'$10^{-2}$',r'$10^{-1}$',r'$10^{0}$',r'$10^{1}$']
+# def plot_results(beta_vals, G_no, G_norm, G_detect,yticks=yticks,yticklabels=yticklabels):
+#     fig, ax = plt.subplots(figsize=(8,6))
+#     ax.plot(beta_vals, G_no, 'o-', label='No')
+#     ax.plot(beta_vals, G_norm, 'x-', label='Norm')
+#     ax.plot(beta_vals, G_detect, 's-', label='Detect')
+#     ax.set_xscale('log')
+#     ax.set_yscale('symlog', linthresh=0.01)
+#     ax.set_xlabel(r'$\beta$')
+#     ax.set_ylabel(r'$G$')
+#     ax.set_yticks(yticks, yticklabels)
+#     ax.legend()
+#     ax.grid(True)
+#
+# beta_vals = [1e-3, 5e-3, 1e-2, 5e-2, 1e-1]
+# G_no=[2.6, 0.2, 0.4, 1.5, 6]
+# G_nor=[0.03, 0.09, 0.2, 0.7, 0.9]
+# G_detect=[0.06, 1e-14, 2e-17, 7e-17, 2e-16]
+#
+#
+#
+# plot_results(beta_vals, G_no, G_nor, G_detect)
+# plt.show()
 
-import xxhash
-import numpy as np
-# h = xxhash.xxh32(seed=1)
-#
-# da = [i for i in range(10)]
-# print(da)
-#
-# print(h)
-# h.update(str(2))
-# seed = 1
-# lj = 2
-# dic = dict(asd=seed,dsg=lj)
-# print(dic['asd'])
-#
-# print(dic)
-#
-# print(h)
-# print(h.intdigest() % 7)
-#
-# h.update(str(99))
-
+from mlxtend.frequent_patterns import fpgrowth
 import pandas as pd
-from collections import Counter
+from pyfpgrowth import find_frequent_patterns
+p = [
+    [1,0,0,1,1,0,1],
+    [1,0,1,1,0,1,1],
+    [1,0,1,0,1,0,1],
+    [1,0,0,0,0,0,1]
+]
+df = pd.DataFrame(p)
 
-x = [1,2,3,3,3,3]
-y=[55,2,21]
+# fp = fpgrowth(df,1, use_colnames=True)
+fp = find_frequent_patterns(p, 4)
 
+# fp['itemsets'] = fp['itemsets'].apply(lambda x: {i+1 for i in x})
+print(fp)
+# print(5000/95)
+# print(52/1052)
+# print(53/1053)
 
-dict = {1:4, 4: 5, 6: 9}
+print(1000/9)
+print(111/1111)
+print(112/1112)
+# max_length = fp['itemsets'].apply(len).max()
 #
-# over = 8
-# df1 = pd.DataFrame(list(dict.items()), columns=['item','count'])
-# df2 = pd.DataFrame({'overall': [over]})
-# df_combined = pd.concat([df1, df2], axis=1)
-# output_file_path = r'11.xlsx'
-# df_combined.to_excel(output_file_path, index=False)
+# # Filter itemsets with the maximum length
+# longest_itemsets = fp[fp['itemsets'].apply(len) == max_length]
+#
+# print(longest_itemsets)
 
-print(310 / 1000000)
+# z = len(itemset)
+# support_count = fp.loc[fp['itemsets'] == itemset, 'support_count'].values[0]
+# print(max_length_itemset)
+# print(z,support_count)
+
+from scipy.special import betainc
+def cal_threshold( q, z, total_user, p):
+    # calclate the threshold tau_z
+    left = 0
+    right = total_user + 1
+    epsilon = 1  # 误差容限
+    while left <= right:
+        mid = (left + right) / 2
+        value = betainc(q ** (z - 1), mid, total_user - mid + 1)
+        if abs(value - p) < epsilon:
+            return mid
+        if value < p:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+q = 0.5
+z = 2
+p = 0.7
+toto = 15
+minx = cal_threshold(q,z,toto,p)
+print(minx)
+[1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0]
+[1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0]
+[1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0]
